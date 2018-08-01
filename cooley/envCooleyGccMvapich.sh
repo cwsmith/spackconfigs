d341a67d343ade96061565875ae6b86d2539b01a
Cameron Smith <smithc11@rpi.edu>
2018-08-01 15:27:38 +0000

    cooley

---
+++ cooley/envCooleyGccMvapich.sh
@@ -0,0 +1,3 @@
+soft add +mvapich2-2.3rc1
+soft add +gcc-7.1.0
+soft add +cmake-3.9.1
---
+++ cooley/setupSpack.sh
@@ -0,0 +1,3 @@
+export SPACK_ROOT=/gpfs/mira-fs1/projects/ATPESC18_Instructors/FASTMath/cwsmith/spack
+export PATH=$SPACK_ROOT/bin:$PATH
+source $SPACK_ROOT/share/spack/setup-env.sh
